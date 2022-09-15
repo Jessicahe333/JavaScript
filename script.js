ce1dfacf365c7addf82f3a8774274945747636af
Xiaoying He <jessicahe333@hotmail.com>
2022-09-14 23:34:02 -0300

    Variaveis & Tipo de Dados

---
+++ script.js
@@ -0,0 +1,48 @@
+var myName = "Jessica";
+
+console.log(myName)
+
+let language = "Javascript";
+
+const pattern = "ECMAScript";
+// 'const' sempre tem que contribuir o valor, e não é mudável no código.
+
+{
+    var age = 32;
+    // Se for 'let', não é permitido exibir mensagem, pq tá fora do escopo.
+}
+
+console.log(age);
+
+var Nome = "Jessica"; // sring
+var Idade = 26; //numero
+var Altura = 1.56; //numero
+
+null
+undefined
+
+var isAluno = false //boolean
+
+//Objeto, array, e function
+var person = {
+    "name": "Luana",
+    "age": 32
+}
+
+var students = ["Simara", "João", "Maria"]
+
+function sum(a,b) {
+    return a+b
+}
+
+//operador typeof [Mostrar o tipo de variável]
+console.log(typeof age)
+console.log(typeof isAluno)
+
+var object = null
+console.log(typeof object)
+
+function soma() {
+    return 5+2
+}
+console.log(typeof soma)
